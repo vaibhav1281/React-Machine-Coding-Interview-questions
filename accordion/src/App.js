4ea008c8b8a670de501ef1e2f40f47abda92138c
Vaibhav_Saroj <93854530+vaibhav1281@users.noreply.github.com>
2024-05-11 15:48:48 +0530

    Accordion

---
+++ accordion/src/App.js
@@ -0,0 +1,129 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [isAccordionOpen, setIsAccordionOpen] = useState(null);
+
+  const handleAccordion = (item) => {
+    if (isAccordionOpen === item) {
+      setIsAccordionOpen(null);
+    } else {
+      setIsAccordionOpen(item);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="accordion1" onClick={() => handleAccordion(1)}>
+        <button className="button">
+          <h3>Accordion 1</h3>
+          {isAccordionOpen === 1 ? (
+            <span className="toggle-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                />
+              </svg>
+            </span>
+          ) : (
+            <span className="toggle-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </span>
+          )}
+        </button>
+        {isAccordionOpen === 1 && (
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+        )}
+      </div>
+      <div className="accordion1" onClick={() => handleAccordion(2)}>
+        <button className="button">
+          <h3>Accordion 2</h3>
+          {isAccordionOpen === 2 ? (
+            <span className="toggle-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                />
+              </svg>
+            </span>
+          ) : (
+            <span className="toggle-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </span>
+          )}
+        </button>
+        {isAccordionOpen === 2 && (
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
